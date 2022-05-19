4dc62862438a6e9395fd994f0d8e08f3088e2d10
Hannah Glazier <hrayglazier@aol.com>
2022-05-19 15:02:10 -0600

    solution

--- ruby/selection_sort.rb
+++ ruby/selection_sort.rb
@@ -1,5 +1,18 @@
+
+
 def selection_sort(arr)
-  # type your code in here
+  # arr.sort
+
+  sorted = []
+
+  until arr.length == 0
+    min = arr.min
+    idx = arr.index(min)
+    sorted << min
+    arr.delete_at(idx)
+
+  end
+sorted
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,10 +24,28 @@ if __FILE__ == $PROGRAM_NAME
 
   # Don't forget to add your own!
 
+  puts "Expecting: [4, 5, 6, 7]"
+  print "=> "
+  print selection_sort([7, 5, 4, 6])
+
+  puts
+
+
   # BENCHMARK HERE, you can print the average runtime
   long_input = []
 
   100.times { long_input << rand }
+
+  start_time = Time.now
+
+  1000.times do 
+    selection_sort([2, 1])
+    selection_sort(long_input)
+  end
+
+  avg_time = (Time.now - start_time) / 2000
+  puts avg_time
+
 end
 
 # Please add your pseudocode to this file
